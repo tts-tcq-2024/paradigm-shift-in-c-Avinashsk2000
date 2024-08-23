29745bbe98720585ca25c566329a58ef07c15250
AVINASH SHIVANAND KUMBAR <84611916+Avinashsk2000@users.noreply.github.com>
2024-08-23 15:50:36 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -4,7 +4,6 @@
 int checkAndPrint(float value, float min, float max, const char* message) {
     if (value < min || value > max) {
         printf("%s", message);
-
     }
     return 1;
 }
@@ -13,6 +12,8 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
     int isTemperatureOk = checkAndPrint(temperature, 0, 45, "Temperature out of range!\n");
     int isSocOk = checkAndPrint(soc, 20, 80, "State of Charge out of range!\n");
     int isChargeRateOk = checkAndPrint(chargeRate, 0, 0.8, "Charge Rate out of range!\n");
+
+return 1;
 
 }
 
